edb75eff48d45d12a305ec746dd231a00d240bd6
ItsHLa <nissanihala@gmail.com>
2024-03-24 18:00:09 +0200

    changes

--- lib/bill_section/scrns/bills_list.dart
+++ lib/bill_section/scrns/bills_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:organize_me/constants.dart';
 import '../widget/bill_Item.dart';
 import '../widget/custom_appBar.dart';
 
@@ -20,11 +21,11 @@ import '../widget/custom_appBar.dart';
               Expanded(
                 child: ListView(
                  children: const [
-                   BillItem(iconBill: Icons.water_drop, BillTitle: "فواتير المياه", paymentBills: "3000" , iconColor: Colors.blueAccent, logic: null,),
-                   BillItem(iconBill: Icons.call, BillTitle: "فواتير الاتصالات", paymentBills: "3000",iconColor: Colors.greenAccent , logic: null,),
-                   BillItem(iconBill: Icons.electric_bolt, BillTitle: "فواتير الكهرباء", paymentBills: "3000",iconColor: Colors.limeAccent,logic: null,),
-                   BillItem(iconBill: Icons.local_gas_station, BillTitle: "فواتير البنزين", paymentBills: "3000",iconColor: Colors.redAccent, logic: null,),
-                   BillItem(iconBill: Icons.gas_meter, BillTitle: "فواتير الغاز ", paymentBills: "3000" , iconColor: Colors.blueGrey, logic: null,)
+                   BillItem(iconBill: Icons.water_drop, BillTitle: "فواتير المياه", paymentBills: "3000" , iconColor: water, logic: null,),
+                   BillItem(iconBill: Icons.call, BillTitle: "فواتير الاتصالات", paymentBills: "3000",iconColor: calls , logic: null,),
+                   BillItem(iconBill: Icons.electric_bolt, BillTitle: "فواتير الكهرباء", paymentBills: "3000",iconColor: elec,logic: null,),
+                   BillItem(iconBill: Icons.local_gas_station, BillTitle: "فواتير البنزين", paymentBills: "3000",iconColor: gasStation, logic: null,),
+                   BillItem(iconBill: Icons.gas_meter, BillTitle: "فواتير الغاز ", paymentBills: "3000" , iconColor: gas, logic: null,)
                  ],
                              ),
               ),
