08b98b9a9395c8dfa7e719bfda0a5ca13b582bac
Haidara <haidaraibrahim963938@gmail.com>
2024-03-25 16:51:23 +0300

    create db.dart, add path to dependencies

---
+++ lib/db.dart
@@ -0,0 +1,33 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static Database? _db;
+
+  static Future<Database?> get db async {
+    if (_db == null) {
+      _db = await intialDb();
+      return _db;
+    } else {
+      return _db;
+    }
+  }
+
+  static intialDb() async {
+    String databasepath = await getDatabasesPath();
+    String path = join(databasepath, 'data.db');
+    Database mydb = await openDatabase(path,
+        onCreate: _onCreate, version: 1, onUpgrade: _onUpgrade);
+    return mydb;
+  }
+
+  static _onUpgrade(Database db, int oldversion, int newversion) {}
+
+  static _onCreate(Database db, int version) async {
+    await db.execute(
+      '''
+          PRAGMA foreign_keys = ON;
+      ''',
+    );
+  }
+}
