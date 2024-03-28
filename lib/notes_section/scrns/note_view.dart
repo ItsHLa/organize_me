f37f0e00d4787c0ea215cd6dc7124ae41b923440
ItsHLa <nissanihala@gmail.com>
2024-03-28 14:03:36 +0200

    changes

--- lib/notes_section/scrns/note_view.dart
+++ lib/notes_section/scrns/note_view.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:organize_me/notes_section/widget/search_widget.dart';
 import '../widget/note_list_view.dart';
 import 'note_task.dart';
 
-class NoteView extends StatelessWidget {
+class NoteView extends StatefulWidget {
   const NoteView({super.key});
+
+  @override
+  State<NoteView> createState() => _NoteViewState();
+}
+
+class _NoteViewState extends State<NoteView> {
+
+   Widget myWidget = const Spacer();
+   Icon myIcon = const Icon(Icons.search_outlined);
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +30,26 @@ class NoteView extends StatelessWidget {
         shape: const StadiumBorder(),
         child: const Icon(Icons.add_task),
       ),
-      body: const Column(
+      body:  Column(
         children: [
+          Row(
+            children: [
+              myWidget,
+              IconButton(onPressed: (){
+                setState(() {
+                  if( myIcon == const Icon(Icons.search_outlined)){
+                    myIcon =  const Icon(Icons.cancel_outlined);
+                    myWidget = const MySearchWidget();
+
+                  }else{
+                    myWidget = const Spacer();
+                    myIcon =  const Icon(Icons.search_outlined);
+                  }
+                });
+               }
+               , icon: myIcon)
+            ],
+          ),
           Expanded(child: TaskListView()),
         ],
       ),
