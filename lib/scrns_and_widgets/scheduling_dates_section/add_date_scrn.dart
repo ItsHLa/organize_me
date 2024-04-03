19eff59c0f51c5f8c95befe9338f0026081256cc
ItsHLa <nissanihala@gmail.com>
2024-04-03 07:04:28 +0300

    changes

--- lib/scrns_and_widgets/scheduling_dates_section/add_date_scrn.dart
+++ lib/scrns_and_widgets/scheduling_dates_section/add_date_scrn.dart
@@ -16,6 +16,37 @@ class _AddDateScrnState extends State<AddDateScrn> {
   TextEditingController endDate = TextEditingController();
   String? DateName;
 
+  int setHour12(int hour) {
+    switch (hour) {
+      case 13:
+        return 1;
+      case 14:
+        return 2;
+      case 15:
+        return 3;
+      case 16:
+        return 4;
+      case 17:
+        return 5;
+      case 18:
+        return 6;
+      case 19:
+        return 7;
+      case 20:
+        return 8;
+      case 21:
+        return 9;
+      case 22:
+        return 10;
+      case 23:
+        return 11;
+      case 24:
+        return 12;
+      default:
+        return hour;
+    }
+  }
+
   void myDatePicker(TextEditingController controller) async {
     DateTime? date = await showDatePicker(
       context: context,
@@ -27,9 +58,11 @@ class _AddDateScrnState extends State<AddDateScrn> {
       context: context,
       initialTime: TimeOfDay.now(),
     );
+
     setState(() {
       controller.text =
-          ' ${date!.day} / ${date!.month}/ ${date!.year} - ${time!.hour} : ${time!.minute}';
+          ' ${date!.day} / ${date!.month} / ${date!.year} - ${setHour12(time!.hour)} : ${time!.minute} ';
+      print(time.hour);
     });
   }
 
