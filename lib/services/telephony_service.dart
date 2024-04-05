00ccac7f11f3eba5c6610ebf0dc2c97037547d7e
ItsHLa <nissanihala@gmail.com>
2024-04-05 21:26:10 +0300

    changes

---
+++ lib/services/telephony_service.dart
@@ -0,0 +1,27 @@
+import 'package:telephony/telephony.dart';
+
+class TelephonyService {
+  static Telephony telephony = Telephony.instance;
+
+  static Future<bool?> askForPermission() async {
+    return await telephony.requestPhoneAndSmsPermissions;
+  }
+
+  static Future<List<SmsMessage>> filterWaterInboxMessages() async {
+    return await telephony.getInboxSms(
+        columns: [SmsColumn.ADDRESS, SmsColumn.BODY],
+        filter: SmsFilter.where(SmsColumn.BODY).like('مياه'));
+  }
+
+  static Future<List<SmsMessage>> filterCallsInboxMessages() async {
+    return await telephony.getInboxSms(
+        columns: [SmsColumn.ADDRESS, SmsColumn.BODY],
+        filter: SmsFilter.where(SmsColumn.BODY).like('اتصالات'));
+  }
+
+  static Future<List<SmsMessage>> filterElectInboxMessages() async {
+    return await telephony.getInboxSms(
+        columns: [SmsColumn.ADDRESS, SmsColumn.BODY],
+        filter: SmsFilter.where(SmsColumn.BODY).like('الكهرباء'));
+  }
+}
