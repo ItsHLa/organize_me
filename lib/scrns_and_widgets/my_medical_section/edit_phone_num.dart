395f094d3eacae3adc2957088196c0dcbd4d4475
ItsHLa <nissanihala@gmail.com>
2024-04-08 23:05:45 +0300

    changes

--- lib/scrns_and_widgets/my_medical_section/edit_phone_num.dart
+++ lib/scrns_and_widgets/my_medical_section/edit_phone_num.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:organize_me/scrns_and_widgets/my_medical_section/cubit/docs_num_cubit.dart';
 import 'package:organize_me/scrns_and_widgets/my_medical_section/widgets/input_phone_num.dart';
 
 class EditPhoneNumber extends StatelessWidget {
@@ -6,6 +8,17 @@ class EditPhoneNumber extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InputPhoneNumber();
+    return BlocListener<DocsNumCubit, DocsNumState>(
+      listener: (context, state) {
+        if (state is AddDocsNumSuccess) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: InputPhoneNumber(
+        onPressed: () {
+          //BlocProvider.of<DocsNumCubit>(context).editPhoneNumber(name, phoneNumber)
+        },
+      ),
+    );
   }
 }
