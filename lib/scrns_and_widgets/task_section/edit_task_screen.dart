3d50ca20866086e0fdbffc8028d4a7f301134713
ItsHLa <nissanihala@gmail.com>
2024-04-25 19:57:32 +0300

    fixed stuff

--- lib/scrns_and_widgets/task_section/edit_task_screen.dart
+++ lib/scrns_and_widgets/task_section/edit_task_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:organize_me/scrns_and_widgets/functionality.dart';
 import 'package:organize_me/scrns_and_widgets/task_section/widgets/add_edit_page_form.dart';
 
 import 'cubit/task_cubit.dart';
@@ -36,42 +37,9 @@ class _EditTaskState extends State<EditTask> {
         child: Form(
           key: taskKey,
           child: TaskDataPageForm(
-            preAlarmValidator: (value) {
-              int interval = int.parse(value!);
-              if (interval.isNegative) {
-                return 'هذا الحقل لا يمكن ان يكون يحوي اعداد سالبة';
-              } else {
-                return null;
-              }
-            },
-            datValidator: (value) {
-              DateTime currentDate = DateTime(DateTime.now().year,
-                  DateTime.now().month, DateTime.now().day);
-              List<String>? parts = value?.split('/');
-              int year = int.parse(parts![2]);
-              int month = int.parse(parts[1]);
-              int day = int.parse(parts[0]);
-              DateTime date = DateTime(year, month, day);
-              if (date.isBefore(currentDate.toUtc())) {
-                return 'لا يمكن ان يكون اليوم قبل اليوم الحالي';
-              } else {
-                return null;
-              }
-            },
-            startTimeValidator: (value) {
-              List<String>? parts = value?.split(':');
-              int hour = int.parse(parts![0]);
-              int minute = int.parse(parts[1]);
-              DateTime currentTime = DateTime.now();
-              DateTime scheduledTime = DateTime(DateTime.now().year,
-                  DateTime.now().month, DateTime.now().day, hour, minute);
-              Duration difference = scheduledTime.difference(currentTime);
-              if (difference.isNegative) {
-                return 'لا يمكن ان يكون الوقت المختار اقل من الوقت الحالي';
-              } else {
-                return null;
-              }
-            },
+            preAlarmValidator: ValidateInputData.checkEditedInterval,
+            datValidator: ValidateInputData.checkEditedDateTime,
+            startTimeValidator: ValidateInputData.checkEditedStartTime,
             saveDate: () async {
               date = (await showDatePicker(
                 context: context,
