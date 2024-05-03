75a4750bb567ecc5f85f2130ddb0e04c9f423e13
ItsHLa <nissanihala@gmail.com>
2024-05-03 17:35:30 +0300

    bill ui and some other stuff

--- lib/scrns_and_widgets/bill_section/bills_view.dart
+++ lib/scrns_and_widgets/bill_section/bills_view.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:organize_me/scrns_and_widgets/bill_section/cubit/bill_cubit.dart';
 import 'package:organize_me/scrns_and_widgets/bill_section/widget/auth_page.dart';
-import 'package:organize_me/scrns_and_widgets/bill_section/widget/electric_bill_list.dart';
 import 'package:organize_me/scrns_and_widgets/bill_section/widget/payment_widget.dart';
-import 'package:organize_me/scrns_and_widgets/bill_section/widget/telecom_list.dart';
-import 'package:organize_me/scrns_and_widgets/bill_section/widget/water_list.dart';
 import 'package:organize_me/scrns_and_widgets/floating_action_button.dart';
 
 import 'monthly_bills.dart';
@@ -28,32 +23,6 @@ class MyBills extends StatelessWidget {
                       context: context,
                       builder: (context) => const PaymentPage());
                 }),
-            SpeedDialChild(
-                onTap: () {
-                  // List<WaterBill> bill=await BlocProvider.of<BillCubit>(context).loadWater();
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (newcontext) => BlocProvider.value(
-                          value: BlocProvider.of<BillCubit>(context),
-                          child: const WaterList())));
-                },
-                child: const Icon(Icons.water_drop)),
-            SpeedDialChild(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (newcontext) => BlocProvider.value(
-                          value: BlocProvider.of<BillCubit>(context),
-                          child: const ElectricList())));
-                },
-                child: const Icon(Icons.electric_bolt)),
-            SpeedDialChild(
-                onTap: () async {
-                  //   List<TelecomBill> bill=await BlocProvider.of<BillCubit>(context).loadTelecom();
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (newcontext) => BlocProvider.value(
-                          value: BlocProvider.of<BillCubit>(context),
-                          child: const TelecomList())));
-                },
-                child: const Icon(Icons.call_end)),
             SpeedDialChild(
                 child: const Icon(Icons.notifications_active_outlined)),
             SpeedDialChild(
--- lib/scrns_and_widgets/bill_section/monthly_bills.dart
+++ lib/scrns_and_widgets/bill_section/monthly_bills.dart
@@ -1,8 +1,15 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:organize_me/constants.dart';
+import 'package:organize_me/scrns_and_widgets/bill_section/widget/electric_bill_list.dart';
+import 'package:organize_me/scrns_and_widgets/bill_section/widget/telecom_list.dart';
+import 'package:organize_me/scrns_and_widgets/bill_section/widget/water_list.dart';
+import 'package:organize_me/services/functionality.dart';
+import 'package:simple_month_year_picker/simple_month_year_picker.dart';
 
 import '../date_time_picker.dart';
+import 'cubit/bill_cubit.dart';
 
 class MonthlyChart extends StatelessWidget {
   const MonthlyChart({super.key});
@@ -29,14 +36,41 @@ class MonthlyChart extends StatelessWidget {
             swapAnimationCurve: Curves.linear, // Optional
           ),
         ),
-        const BillCategorySpending(
-            sum: 3500, color: yellow, value: 50, title: 'الكهرباء'),
+        BillCategorySpending(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (newcontext) => BlocProvider.value(
+                      value: BlocProvider.of<BillCubit>(context),
+                      child: const ElectricList())));
+            },
+            sum: 3500,
+            color: yellow,
+            value: 50,
+            title: 'الكهرباء'),
         const Divider(),
-        const BillCategorySpending(
-            sum: 3500, color: blue, value: 20, title: 'المياه'),
+        BillCategorySpending(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (newcontext) => BlocProvider.value(
+                      value: BlocProvider.of<BillCubit>(context),
+                      child: const WaterList())));
+            },
+            sum: 3500,
+            color: blue,
+            value: 20,
+            title: 'المياه'),
         const Divider(),
-        const BillCategorySpending(
-            sum: 3500, color: green, value: 30, title: 'الاتصالات')
+        BillCategorySpending(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (newcontext) => BlocProvider.value(
+                      value: BlocProvider.of<BillCubit>(context),
+                      child: const TelecomList())));
+            },
+            sum: 3500,
+            color: green,
+            value: 30,
+            title: 'الاتصالات')
       ],
     );
   }
@@ -48,18 +82,21 @@ class BillCategorySpending extends StatelessWidget {
       required this.color,
       required this.value,
       required this.title,
-      required this.sum});
+      required this.sum,
+      required this.onTap});
 
   final Color color;
   final double value;
   final String title;
   final double sum;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap: onTap,
       title: Text(title),
-      subtitle: Text('مجموع فواتيرك : $sum'),
+      subtitle: Text(' مجموع فواتيرك : $sum'),
       leading: SizedBox(
         height: 35,
         width: 35,
@@ -78,50 +115,62 @@ class BillCategorySpending extends StatelessWidget {
   }
 }
 
-class MySearchBar extends StatelessWidget {
+class MySearchBar extends StatefulWidget {
   const MySearchBar({super.key});
+
+  @override
+  State<MySearchBar> createState() => _MySearchBarState();
+}
+
+class _MySearchBarState extends State<MySearchBar> {
+  DateTime? selectedDate;
+
+  String date = '';
+  GlobalKey<FormState> key = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: SizedBox(
-          height: 45,
-          child: MyDatePicker(
-            labelText: '',
-            onTap: () {},
-            controller: TextEditingController(text: ''),
-          )),
+      child: Row(
+        children: [
+          SizedBox(
+              height: 48,
+              width: 290,
+              child: Form(
+                key: key,
+                child: MyDatePicker(
+                  labelText: '',
+                  validator: (value) {
+                    if (selectedDate != null &&
+                        (selectedDate!.year > DateTime.now().year)) {
+                      return 'لا يمكن السنة المختارة ان تكون اكبر من السنة الحالية';
+                    }
+                  },
+                  onTap: () async {
+                    selectedDate =
+                        await SimpleMonthYearPicker.showMonthYearPickerDialog(
+                      context: context,
+                      disableFuture: false,
+                      selectionColor: blue,
+                      monthTextStyle: const TextStyle(),
+                      yearTextStyle: const TextStyle(),
+                      titleTextStyle: const TextStyle(fontSize: 20),
+                    );
+                    setState(() {
+                      date = '${selectedDate?.year} / ${selectedDate?.month}';
+                    });
+                  },
+                  controller: TextEditingController(text: date),
+                ),
+              )),
+          IconButton(
+              onPressed: () {
+                if (ValidateInputData.validateField(key)) {}
+              },
+              icon: const Icon(Icons.search_outlined))
+        ],
+      ),
     );
   }
 }
-/*
-*
-* TextField(
-
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              gapPadding: 8,
-              borderRadius: BorderRadius.circular(18)
-            ),
-            prefixIcon: IconButton(
-              onPressed: (){
-                print('object');
-              },
-              icon: Icon(Icons.search),
-            )
-          ),
-        ),
-*
-* */
-
-/*
-*
-*  var selected = await showMonthYearPicker(
-              context: context,
-              initialDate: DateTime.now(),
-              firstDate: DateTime(2024),
-              lastDate: DateTime(3000),
-            );
-*
-* */
