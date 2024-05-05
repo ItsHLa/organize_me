d9a51fb010c61f49c8a3974d313656df253c0c7f
Haidara <haidaraibrahim963938@gmail.com>
2024-05-06 02:27:52 +0300

    remove requests

--- lib/services/api_calls.dart
+++ lib/services/api_calls.dart
@@ -1,11 +1,10 @@
-import 'package:requests/requests.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:organize_me/user.dart';
 import 'package:organize_me/constants.dart';
 
-Future<Response> addUser(User user) async {
-  var r = await Requests.post(
-    "$baseUrl/addUser/",
+Future<http.Response> addUser(User user) async {
+  var r = await http.post(
+    Uri.parse("$baseUrl/addUser/"),
     body: {
       "email": user.email,
       "password": user.password,
@@ -15,7 +14,9 @@ Future<Response> addUser(User user) async {
   return r;
 }
 
-Future<Response> getBills(int userId, String type) async {
-  var r = await Requests.get("$baseUrl/getBills/$type/$userId/");
+Future<http.Response> getBills(int userId, String type) async {
+  var r = await http.get(
+    Uri.parse("$baseUrl/getBills/$type/$userId/"),
+  );
   return r;
 }
